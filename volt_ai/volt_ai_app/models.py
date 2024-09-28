9b97582966c20783edee9b24e98761e90d79c0c3
Dereck Portela <dereck_alexander@outlook.com>
2024-09-28 18:35:16 -0300

    add login and point and create user model

--- volt_ai/volt_ai_app/models.py
+++ volt_ai/volt_ai_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    username = models.CharField(max_length=15, unique=True)
+    password = models.CharField(max_length=128)
+    
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
--- volt_ai/volt_ai_app/views.py
+++ volt_ai/volt_ai_app/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .models import User
+from volt_ai_app.serializers.serializers import UserSerializer
 
-@api_view(['GET'])
+@api_view(['POST'])
 def login(request):
-    return Response({"message": "Endpoint is working fine"}, status = status.HTTP_200_OK)
+    username = request.data.get("username")
+    raw_password = request.data.get("password")
+
+    if not username or not raw_password:
+        return Response({"message": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        user = User.objects.get(username=username)
+    
+    except User.DoesNotExist:
+        return Response({"message": "The credentials are invalid or the user does not exist"}, status = status.HTTP_400_BAD_REQUEST) 
+    
+
+    except Exception as e:
+        return Response({"message": f"An unexpected error occurred: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    serializer = UserSerializer(user)
+    if user.check_password(raw_password):
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    else:
+        return Response({"message": "The credentials are invalid or the user does not exist"}, status = status.HTTP_400_BAD_REQUEST)
+  
+    
+
+    
